698bd9a1f258d339f3e2432de9c3a795d5a6f2c2
Kachan Sergey <s.kachan@silencatech.com>
2021-03-28 10:19:41 +0300

    Add message validation. Add isActive flag for survey. Add /can_vote and /vote methods

--- src/controllers/api.js
+++ src/controllers/api.js
@@ -1,14 +1,15 @@
 require('dotenv').config();
-const { IPFS_SERVER, ROOT_DIR, ARAGON_ORG, ETH_NETWORK } = process.env;
+const { IPFS_SERVER, ROOT_DIR, ARAGON_ORG, ETH_NETWORK, INFURA_URL } = process.env;
 
-const { utils: { isAddress, fromWei } } = require('web3');
+const { utils: { isAddress } } = require('web3');
 const Web3 = require('web3');
 const { connect } = require('@aragon/connect');
 const { default: connectTokens } = require('@aragon/connect-tokens');
 const createClient = require('ipfs-http-client')
+const Erc20Abi = require('../erc20.abi.json');
 
 let web3 = new Web3(
-    new Web3.providers.WebsocketProvider("wss://rinkeby.infura.io/ws/v3/6ec291dd1fe540948986c1ce5267aafb")
+    new Web3.providers.WebsocketProvider(INFURA_URL)
 );
 
 const client = createClient(IPFS_SERVER || 'http://127.0.0.1:5002')
@@ -35,7 +36,7 @@ const getProcessor = type => {
 
             return holders.map(h => h.address).indexOf(address) !== -1;
         },
-        getSurveyData: request => {
+        getSurveyData: (ts, request) => {
             const start = +request.start;
             const end = +request.end;
             const block = +request.block;
@@ -64,14 +65,36 @@ const getProcessor = type => {
                 block,
                 token,
                 name,
-                price
+                price,
+                isActive: ts >= start && ts <= end,
+                id: ts
             };
         },
     }
 };
 
-const validateSign = (address, data, sign) => {
+const isValidateSign = (address, data, sign) => {
+    const signer = web3.eth.accounts.recover(data, sign);
 
+    return signer.toLowerCase() === address.toLowerCase();
+};
+
+const asyncGenToJson = async gen => {
+    let content = '';
+    for await (const row of gen) {
+        content += row.toString();
+    }
+
+    return JSON.parse(content);
+};
+
+const getTokenBalance = async (survey, address) => {
+    const contract = new web3.eth.Contract(Erc20Abi, survey.token);
+
+    const decimals = await contract.methods.decimals().call();
+    const rawBalance = await contract.methods.balanceOf(address).call({}, survey.block);
+
+    return rawBalance * Math.pow(10, -1*decimals);
 };
 
 module.exports = app => {
@@ -89,15 +112,18 @@ module.exports = app => {
                 const dir = await client.files.ls(`/${ROOT_DIR}`);
                 const surveys = [];
 
+                const ts = new Date().getTime();
+
                 for await (const entity of dir) {
                     const data = await client.files.read(`/${ROOT_DIR}/${entity.name}/info`);
-                    let content = '';
-                    for await (const row of data) {
-                        content += row.toString();
-                    }
 
                     try {
-                        surveys.push(JSON.parse(content));
+                        const survey = await asyncGenToJson(data);
+                        surveys.push({
+                            ...survey,
+                            isActive: ts >= survey.start && ts <= survey.end,
+                            id: entity.name,
+                        });
                     } catch (err) {
                         console.log('[E] parse survey data', err.toString());
                     }
@@ -119,24 +145,71 @@ module.exports = app => {
         // Create new survey
         app.post('/survey', async (req, res) => {
             try {
-                const {type, address, data, sign} = req.body;
+                const { type, address, data, sign } = req.body;
                 const processor = getProcessor(type);
-                const id = new Date().getTime();
+
+                const ts = new Date().getTime();
 
                 if (!(await processor.canCreateSurvey(address))) {
                     throw 'You can\'t create survey';
                 }
 
-                await validateSign(address, data, sign);
+                if (!(await isValidateSign(address, data.name, sign))) {
+                    throw 'Invalid signature';
+                }
 
-                const survey = await processor.getSurveyData(req.body);
+                const survey = await processor.getSurveyData(ts, req.body);
 
-                await client.files.mkdir(`/${ROOT_DIR}/${id}`);
-                await client.files.write(`/${ROOT_DIR}/${id}/info`, JSON.stringify(survey), {create: true});
+                await client.files.mkdir(`/${ROOT_DIR}/${survey.id}`);
+                await client.files.write(`/${ROOT_DIR}/${survey.id}/info`, JSON.stringify(survey), {create: true});
 
                 res.json({
                     result: true,
-                    survey: null
+                    survey
+                });
+            } catch(err) {
+                res.json({
+                    result: false,
+                    code: err.code || 0,
+                    error: err.toString(),
+                });
+            }
+        });
+
+        app.get('/survey/:id/can_vote', async (req, res) => {
+            try {
+                const { id } = req.params;
+                const { address } = req.query;
+
+                const data = await client.files.read(`/${ROOT_DIR}/${id}/info`);
+                const survey = await asyncGenToJson(data);
+
+                if (!survey) {
+                    throw 'Survey not founded';
+                }
+
+                if (!survey.token) {
+                    throw 'Invalid survey token';
+                }
+
+                if (!survey.block) {
+                    throw 'Invalid survey block';
+                }
+
+                const balance = await getTokenBalance(survey, address);
+
+                if (balance <= 0) {
+                    throw `No funds on block "${survey.block}"`;
+                }
+
+                const ts = new Date().getTime();
+
+                if (ts < survey.start || ts > survey.end) {
+                    throw 'Survey isn\'t active';
+                }
+
+                res.json({
+                    result: true,
                 });
             } catch(err) {
                 res.json({
@@ -149,10 +222,54 @@ module.exports = app => {
 
         // Vote
         app.post('/survey/:id/vote', async (req, res) => {
-            res.json({
-                result: true,
-                vote: null
-            });
+            try {
+                const { id } = req.params;
+                const { address, sign } = req.query;
+
+                const data = await client.files.read(`/${ROOT_DIR}/${id}/info`);
+                const survey = await asyncGenToJson(data);
+
+                if (!survey) {
+                    throw 'Survey not founded';
+                }
+
+                if (!survey.token) {
+                    throw 'Invalid survey token';
+                }
+
+                if (!survey.block) {
+                    throw 'Invalid survey block';
+                }
+
+                const balance = await getTokenBalance(survey, address);
+
+                if (balance <= 0) {
+                    throw `No funds on block "${survey.block}"`;
+                }
+
+                const ts = new Date().getTime();
+
+                if (ts < survey.start || ts > survey.end) {
+                    throw 'Survey isn\'t active';
+                }
+
+                if (!(await isValidateSign(address, id, sign))) {
+                    throw 'Invalid signature';
+                }
+
+                await client.files.mkdir(`/${ROOT_DIR}/${survey.id}`);
+                await client.files.write(`/${ROOT_DIR}/${survey.id}/${address}`, JSON.stringify({ ts, address, sign }), {create: true});
+
+                res.json({
+                    result: true,
+                });
+            } catch(err) {
+                res.json({
+                    result: false,
+                    code: err.code || 0,
+                    error: err.toString(),
+                });
+            }
         });
     });
 };
